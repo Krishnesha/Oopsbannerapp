c9788cbd608c5848db4a646c2192be8d0a5d0016
Krishnesha <krishneshasarma@gmail.com>
2026-03-06 18:26:46 +0530

    Uc6Oopsprint

---
+++ Uc6printOops.java
@@ -0,0 +1,14 @@
+public class Uc6printOops {
+    public static String[] get0Pattern() {
+        return new String[] {
+            "   ***   ",
+            " **   ** ",
+            "**     **",
+            "**     **",
+            "**     **",
+            " **   ** ",
+            "   ***   " 
+        };
+
+    } 
+} 
