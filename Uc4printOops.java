de630b5f4828db285d4da1b86a412c4e287c36d4
Krishnesha <krishneshasarma@gmail.com>
2026-03-11 14:22:31 +0530

    Uc7Oopsprint

--- Uc4printOops.java
+++ Uc4printOops.java
@@ -4,13 +4,13 @@ public class Uc4printOops{
 
         lines = new String[7];
 
-        lines[0] = String.join(" ", "   ***   ", "   **** ");
-        lines[1] = String.join(" ", " **   ** ", " **     ");
-        lines[2] = String.join(" ", "**     **", "**      ");
-        lines[3] = String.join(" ", "**     **", "  ***** ");
-        lines[4] = String.join(" ", "**     ** ","      **");
-        lines[5] = String.join(" ", " **   **  "," **   **");
-        lines[6] = String.join(" ", "   ***    ","  ***** ");
+        lines[0] = String.join(" ", "   ***   ", "   ***   ", " ******* ", "  *****  ");
+        lines[1] = String.join(" ", " **   ** ", " **   ** ", " *     * ", " **      ");
+        lines[2] = String.join(" ", "**     **", "**     **", " *     * ", "**       ");
+        lines[3] = String.join(" ", "**     **", "**     **", " *   * * ", "  *****  ");
+        lines[4] = String.join(" ", "**     ** ","**     **", " *       ", "      ** ");
+        lines[5] = String.join(" ", " **   **  "," **   **  "," *       ", " **   ** ");
+        lines[6] = String.join(" ", "   ***    ","   ***    "," *       ", "  *****  ");
       
     }
 }
